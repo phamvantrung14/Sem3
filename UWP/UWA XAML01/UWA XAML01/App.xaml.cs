23d54555294217885c04830b726e8278f79bafb6
trung <phamvantrung14111994@gmail.com>
2020-08-13 20:57:05 +0700

    example2

--- UWP/UWA XAML01/UWA XAML01/App.xaml.cs
+++ UWP/UWA XAML01/UWA XAML01/App.xaml.cs
@@ -66,7 +66,7 @@ namespace UWA_XAML01
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(AlignWithPanel), e.Arguments);
+                    rootFrame.Navigate(typeof(ScrollViewer), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
