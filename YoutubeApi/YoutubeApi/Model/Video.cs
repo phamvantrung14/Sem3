4d549909ba16c5aed7ff24a1dc64585f3d24d60b
trung <phamvantrung14111994@gmail.com>
2020-08-21 18:56:07 +0700

    youtube_App

---
+++ YoutubeApi/YoutubeApi/Model/Video.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeApi.Model
+{
+    public class Video
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Img { get; set; }
+        public string Description { get; set; }
+        public Uri ThumbnailUri { get; set; }
+        public MyToolkit.Multimedia.YouTube.YouTubeUri VideoUri { get; set; }
+        public DateTime? PubDate { get; set; }
+    }
+}
---
+++ YoutubeApi/YoutubeApi/VideoPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using MyToolkit.Multimedia;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using YoutubeApi.Model;
+using static MyToolkit.Multimedia.YouTube;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace YoutubeApi
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class VideoPage : Page
+    {
+        Google.Apis.YouTube.v3.Data.Video video;
+        public VideoPage()
+        {
+            this.InitializeComponent();
+        }
+       
+        public object Youtube { get; private set; }
+
+        protected override async  void OnNavigatedTo(NavigationEventArgs e)
+        {
+            string YouTubeId = "eVkO-vbcKOs";
+           
+           
+            try
+            {
+                if (NetworkInterface.GetIsNetworkAvailable())
+                {
+                    video = e.Parameter as Google.Apis.YouTube.v3.Data.Video;
+                    
+                    YouTubeUri url = await YouTube.GetVideoUriAsync(video.Id, YouTubeQuality.Quality480P);
+                    MediaElt.Source = url.Uri;
+                    MediaElt.Play();
+                    
+                   
+                }
+                else
+                {
+                    MessageDialog message = new MessageDialog("You are not connected to Internet");
+                    await message.ShowAsync();
+                    this.Frame.GoBack();
+                }
+            }
+            catch { }
+            base.OnNavigatedTo(e);
+        }
+        private void btnHomePage_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage), new object());
+        }
+    }
+}
